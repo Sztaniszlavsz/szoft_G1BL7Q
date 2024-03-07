72fbcdb84c9280d2ccadb3132575c5ead224ad6c
Sztaniszlavsz <szabolcs.sztaniszlav1@gmail.com>
2024-03-07 10:31:27 +0100

    commit

---
+++ Zh1_gyak/RácsEgySora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zh1_gyak
+{
+    internal class RácsEgySora
+    {
+        public int Sorszám { get; set; }
+
+        public int VéletlenSzám { get; set; }
+
+        public int Fibo { get; set; }
+    }
+}
